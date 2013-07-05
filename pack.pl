4b54b12543e10a538b9c41f0b7d9b44c3a70e176
Michael Hendricks <michael@ndrix.org>
2013-07-05 15:23:17 -0600

    Fill out pack file omissions

--- pack.pl
+++ pack.pl
@@ -1,8 +1,12 @@
 name(uri_qq).
 version('0.0.1').
-title('').
+title('URI quasiquotations').
+
+requires(function_expansion).
+
 author('Michael Hendricks','michael@ndrix.org').
 packager('Michael Hendricks','michael@ndrix.org').
 maintainer('Michael Hendricks','michael@ndrix.org').
+
 home('http://packs.ndrix.com/uri_qq/index.html').
 download('http://packs.ndrix.com/uri_qq/uri_qq-0.0.1.tgz').
